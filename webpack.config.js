7af7fb27034736d91d697f43f291ddfb51981d33
EugeneHlushko <jhlushko@gmail.com>
2018-04-12 09:31:35 +0300

    docs(plugins) Add mini-css-extract-plugin remove ETWP

--- webpack.config.js
+++ webpack.config.js
@@ -119,6 +119,7 @@ module.exports = (env) => ({
         'development/release-process': '/contribute/release-process/',
         'development/how-to-write-a-loader': '/contribute/writing-a-loader/',
         'development/how-to-write-a-plugin': '/contribute/writing-a-plugin/',
+        'extract-text-webpack-plugin': '/plugins/mini-css-extract-plugin/'
       },
     }),
   ]
